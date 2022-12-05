b77d7556e06dc4fdc553cc4a16efbe0a3b608027
victoresende19 <victoresende19@gmail.com>
2022-12-04 22:25:12 -0300

    Rouge metric

--- summarization.py
+++ summarization.py
@@ -45,11 +45,12 @@ def english_model():
 
 
 @st.cache(hash_funcs={StringIO: StringIO.getvalue}, allow_output_mutation=True, suppress_st_warning=True, show_spinner=False, ttl=24*3600, max_entries=2)
-def HARIM_accuracy():
-    summarizer = pipeline("summarization", model="knkarthick/MEETING_SUMMARY")
+def ROUGE_accuracy():
+    summarizer = evaluate.load('rouge')
     return summarizer
 
-#@st.cache(hash_funcs={StringIO: StringIO.getvalue}, allow_output_mutation=True, suppress_st_warning=True, show_spinner=False, ttl=24*3600, max_entries=2)
+
+@st.cache(hash_funcs={StringIO: StringIO.getvalue}, allow_output_mutation=True, suppress_st_warning=True, show_spinner=False, ttl=24*3600, max_entries=2)
 def portuguese_summarization(text: str) -> str:
     """
     Sumariza o texto disponibilizado (em português)
@@ -58,16 +59,14 @@ def portuguese_summarization(text: str) -> str:
     """
 
     tokenizer, model_pt = portuguese_model()
-    inputs = tokenizer.encode(text, max_length=512,
-                              truncation=True, return_tensors='pt')
-    summary_ids = model_pt.generate(
-        inputs, max_length=256, min_length=32, num_beams=5, no_repeat_ngram_size=3, early_stopping=True)
+    inputs = tokenizer.encode(text, max_length=512, truncation=True, return_tensors='pt')
+    summary_ids = model_pt.generate(inputs, max_length=256, min_length=32, num_beams=5, no_repeat_ngram_size=3, early_stopping=True)
     summary = tokenizer.decode(summary_ids[0])
 
     return summary
 
 
-#@st.cache(hash_funcs={StringIO: StringIO.getvalue}, allow_output_mutation=True, suppress_st_warning=True, show_spinner=False, ttl=24*3600, max_entries=2)
+@st.cache(hash_funcs={StringIO: StringIO.getvalue}, allow_output_mutation=True, suppress_st_warning=True, show_spinner=False, ttl=24*3600, max_entries=2)
 def english_summarization(text: str) -> str:
     """
     Sumariza o texto disponibilizado (em inglês)
@@ -76,11 +75,10 @@ def english_summarization(text: str) -> str:
     """
 
     summarizer = english_model()
-    # return summarizer(text, max_length=130, min_length=30, do_sample=False)[0]['summary_text']
     return summarizer(text)
 
 
-#@st.cache(hash_funcs={StringIO: StringIO.getvalue}, allow_output_mutation=True, suppress_st_warning=True, show_spinner=False, ttl=24*3600, max_entries=2)
+@st.cache(hash_funcs={StringIO: StringIO.getvalue}, allow_output_mutation=True, suppress_st_warning=True, show_spinner=False, ttl=24*3600, max_entries=2)
 def acc_summarization(texto: str, resumo: str) -> str:
     """
     Retorna a acurácia do resumo por meio da métrica Harim.
@@ -88,11 +86,11 @@ def acc_summarization(texto: str, resumo: str) -> str:
     retorna - acuracia: acuracia do resumo
     """
 
-    HARIM = HARIM_accuracy()
+    ROUGE = ROUGE_accuracy()
 
     texto_cru = [texto]
     texto_resumido = [resumo]
-    acuracia = HARIM.compute(references=texto_cru, predictions=texto_resumido)
+    acuracia = ROUGE.compute(predictions=texto_resumido, references=texto_cru)
 
     return round(acuracia[0], 4)
 
@@ -137,24 +135,19 @@ st.sidebar.markdown('')
 st.sidebar.markdown("<h4 style='text-align: center; color: black;'> Contate o autor por meio do QRCode </h4>", unsafe_allow_html=True)
 st.sidebar.image(Image.open('Images/QRCode.png'), caption='LinkedIn Victor Resende', width=230)
 
-
 text_type = st.selectbox('Que maneira gostaria de resumir seu texto?', ('Escolha as opções', 'Resumo escrito', 'Resumo em PDF'))
 
 if text_type == 'Resumo escrito':
     form = st.form(key='my_form')
     language = form.selectbox('Qual a língua do PDF?', ('Português', 'Inglês'))
-    text = form.text_area(
-        "Texto a ser resumido:",
-        height=300,
-        placeholder='Escreva aqui...'
-    )
+    text = form.text_area("Texto a ser resumido:", height=300, placeholder='Escreva aqui...')
     submit_button = form.form_submit_button(label='✨ Resumir!')
 
     if submit_button:
         with st.spinner('Resumindo...'):
             st.markdown("<h4 style='text-align: center; color: black;'> Resumo </h4>",  unsafe_allow_html=True)
             st.info(f"{display_summarization(text, language).replace('<pad> ', '').replace('</s>', '')}")
-            #st.markdown(f"<p> Acurácia (<a href='https://huggingface.co/spaces/NCSOFT/harim_plus'>HaRiM</a>): {acc_summarization(text, display_summarization(text, language))}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p> Acurácia (<a href='https://huggingface.co/spaces/NCSOFT/harim_plus'>HaRiM</a>): {acc_summarization(text, display_summarization(text, language))}</p>", unsafe_allow_html=True)
 
 elif text_type == 'Resumo em PDF':
     form = st.form(key='my_form')
@@ -167,4 +160,4 @@ elif text_type == 'Resumo em PDF':
         with st.spinner('Resumindo...'):
             st.markdown("<h4 style='text-align: center; color: black;'> Resumo </h4>", unsafe_allow_html=True)
             st.info(f"{display_summarization(pdf, language).replace('<pad> ', '').replace('</s>', '')}")
-            #st.markdown(f"<p> Acurácia (<a href='https://huggingface.co/spaces/NCSOFT/harim_plus'>HaRiM</a>): {acc_summarization(pdf, display_summarization(pdf, language))}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p> Acurácia (<a href='https://huggingface.co/spaces/NCSOFT/harim_plus'>HaRiM</a>): {acc_summarization(pdf, display_summarization(pdf, language))}</p>", unsafe_allow_html=True)
